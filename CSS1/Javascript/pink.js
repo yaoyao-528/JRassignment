9276c62884bd837fb2935f566664cf5897b6df15
yaoyao-528 <yfen90@gmail.com>
2025-12-16 19:50:04 +1100

    new version from

---
+++ CSS1/Javascript/pink.js
@@ -0,0 +1,48 @@
+/* shift +alt+a */
+/* ; 结束符 */
+/* input + output */
+document.write('');
+alert('');
+console.log(''); //给程序员看的if it is right or fault
+
+prompt('please type your name') //有对话框
+
+/* 变量 */
+let age = 18;
+age = 19; // let 不允许多次声明
+
+temp = num1;
+num1 = num2;
+num2 = temp;
+
+
+/* 数组  */
+let name =[12, 13, 14, 15];
+console.log(arr[1]);
+/* 常量 never change , const*/
+const PI = 3.14;
+console.log(PI);
+
+/* 数据类型number, string, boolean, null, undefined, object */
+console.log(1+1);
+console.log(4%2) //余数 0
+/* 自增 ----*/
+let i = 1;
+++i; //前面增加 
+console.log(i);
+i++; //后面自增都一样n=n+1
+i = 1;
+console.log(++i +1)  //3
+let i =1 
+console.log(i++ + 1)//2
+
+/* 运算优先级 */
+&& then ||
+比较运算符 > 逻辑运算
+
+
+
+
+
+
+
