d5eb3c2f0feeb3f8312ce2f7ca79821faf094b87
Yaoyao Feng <yfen90@gmail.com>
2026-01-15 17:30:55 +1100

    errorMessage

--- CareerMateAI/script.js
+++ CareerMateAI/script.js
@@ -25,36 +25,89 @@ if(backToButton) {
   })
 }
 
-// validation ? Object 存起来---
-// const Formvalidator = {
-//   rules: {
-//     fullname: {
-//       required: true,
-//       minLength: 2,
-//       message: 'Please enter your full name at least '
 
 
 
-//     }
-//     email: {  
-//       required: true,
-//       pattern://Regex regular pattern,
-//       message: 'please enter a valid email address'
 
 
-//     },
-//     message: {
-//       required: true,
-//       minLength: 10,
-//       message: 'Message must be at lease 10 characters long'
-//     },
-//     terms: {
-//       required: true,
-//       message: 'You must agree to the terms of service'
-//     }
 
-//   },
-//   validField(field) {
+//validation ? Object 存起来---
+//思考一下搞什么样的validation, 先用object 存起来
+const Formvalidator = {
+  rules: {
+    fullname: {
+      required: true,
+      minLength: 2,
+      message: 'Please enter your full name at least '
+    },
+    email: {  
+      required: true,
+      pattern:  /^[^\s@]+@[^\s@]+\.[^\s@]+$/, //Regex regular pattern,
+      message: 'please enter a valid email address'
+    },
+    message: {
+      required: true,
+      minLength: 10,
+      message: 'Message must be at lease 10 characters long'
+    },
+    terms: {
+      required: true,
+      message: 'You must agree to the terms of service'
+    }
 
-//   }
-// }
+  },
+
+  validateField(field) {
+        const fieldName = field.name;
+        const fieldvalue = field.type === 'checkbox' ? field.checked: field.value.trim()
+        const rule = this.rules[fieldName]
+        
+        if (! rule) return true;
+        if(rule.required) {
+            if (field.type === 'checkbox' && !fieldvalue) {
+                  console.log('error')
+                  this.showError(field, rule.message)
+                  return false
+              }
+              if (field.type !== 'checkbox' && !fieldvalue) {
+                  console.log('this is another error')
+                  this.showError(field, rule.message)
+                  return false
+              }        
+        }
+  },
+
+  showError(field, message) {
+      const formGroup = field.closest ('.form-group') || field.closest('.form-checkbox')
+
+      formGroup.classList.remove('success')
+      formGroup.classList.add('error')
+
+      const existingError = formGroup.querySelector('.error -message')
+      if(existingError){
+        existingError.remove()
+      }
+
+      const errorDiv = document.createElement('div')
+      errorDiv.className = 'error-message'
+      errorDiv.innerHTML = `<span>◮</span>${message}`
+
+      if (field.type ==='checkbox') {
+        formGroup.appendChild(errorDiv);
+      } else {
+        field.parentNode.appendChild(errorDiv);
+      }
+
+  }
+}
+const contactForm = document.querySelector('.contact-form')
+if (contactForm) {
+  const validateField = contactForm.querySelectorAll('[name="fullname"], [name="email"],[name="message"],[name="terms"]')
+  //遍历
+  validateField.forEach(field =>{
+      field.addEventListener('blur', function(){
+          Formvalidator.validateField(this);
+
+      })
+  })
+}
